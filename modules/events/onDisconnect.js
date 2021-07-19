6d8515a6a765f53281514cc4d6b4834608a95bd9
letconst <2000takeru93@gmail.com>
2021-07-19 23:08:05 +0900

    [Fix] 退出処理時に参加人数が減少していなかったのを修正

--- modules/events/onDisconnect.js
+++ modules/events/onDisconnect.js
@@ -12,6 +12,7 @@ module.exports = (data, sender, server) => {
 
         if (clients.hasOwnProperty(uuid)) {
             delete clients[uuid];
+            clients.length--;
             console.log(`${sender.address}:${sender.port} is disconnected`);
         } else {
             console.error(`UUID "${uuid}" isn't exist in connected clients`);
