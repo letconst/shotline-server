63b94cfd435d10f9a7cef9d14014c141ec8cd759
letconst <2000takeru93@gmail.com>
2021-07-20 22:34:08 +0900

    [Update] Discordボットに稼働状況を送信するように

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const IS_DEBUG = false;
+
+const fetch       = require('node-fetch');
 const initializer = require('./modules/utils/initializer');
 
 initializer.logger();
@@ -17,11 +20,15 @@ const EventHandler = require('./modules/EventHandler');
 global.clients = [];
 
 initializer.dotenv();
-const { PORT } = process.env;
+const { PORT, DISCORD_BOT_URL } = process.env;
 
 server.on('listening', () => {
     const address = server.address();
     console.info(`Server listening on ${address.address}:${address.port}`);
+
+    // Discordボットにステータス設定
+    const type = IS_DEBUG ? 'debug' : 'start';
+    postSetActivity({ type: type });
 });
 
 server.on('message', (msg, info) => {
@@ -33,4 +40,34 @@ server.on('error', (err) => {
     server.close();
 });
 
+server.on('close', () => {
+    console.info('Server closed');
+    process.exit(0);
+});
+
 server.bind(PORT);
+
+// 終了時にDiscordボットのステータス解除
+process
+    .on('exit', async () => await postSetActivity({ type: 'stop' }))
+    .on('SIGTERM', async () => await postSetActivity({ type: 'stop' }))
+    .on('SIGINT', async () => {
+        await postSetActivity({ type: 'stop' });
+        process.exit(0);
+    });
+
+/**
+ * Discordボットのアクティビティへの稼働状況設定用リクエストをPOSTする
+ * @param {Object} body activityOptions
+ * @return {*|Promise<Response>}
+ */
+function postSetActivity(body) {
+    return fetch(`${DISCORD_BOT_URL}/setActivity`, {
+        method : 'POST',
+        headers: {
+            'Accept'      : 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body   : JSON.stringify(body)
+    });
+}
